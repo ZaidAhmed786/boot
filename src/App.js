35b287dc69c59150b55aa0df10e93884d0bb3efd
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-14 16:53:07 +0500

    check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Dinner from './dinner.js';
 function App() {
-  return   <Dinner dishName='biryani' sweetdish='rasmalai'></Dinner>
+  return 
+  (
+  <div className='App'>
+  <Dinner dishName='biryani' sweetdish='rasmalai'></Dinner>
+  <Dinner dishName='biryani' sweetdish='rasmalai'></Dinner>
     
-    
+   </div>
+  )
   
 }
 
