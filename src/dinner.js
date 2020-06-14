de45894b25f0e94133ddd58578a817e95f323607
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-14 16:23:20 +0500

    update React to dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -1,9 +1,11 @@
-import react from 'react';
+import React from 'react';
 
-function dinner(props) {
+function Dinner(props) {
 
 return (
 <h1>today we are serving {props.dishName}</h1>
 )
 
 }
+
+export default Dinner;
