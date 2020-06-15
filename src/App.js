eae051a7733b64d017223bec244f18c8e4322c03
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-15 17:20:02 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import C from './counter.js';
 function App() {
-  let [count , setCount] = useState[1];
+  let [count, setCount] = useState(1);
 
   return ( 
     <div>
-  <C counter={count } />
+  <C counter={count} />
   <br/>
-  <button onClick={()=> setCount (count + 1) }>update counter</button>
+  <button onClick={() => setCount(count + 1)}>update counter</button>
 
     </div>
   );
