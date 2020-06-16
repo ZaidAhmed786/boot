7cff35fd33062012f39dc46867a907b5d3fd8bb2
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-16 16:13:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 import C from './counter.js';
 function App() {
   let [count, setCount] = useState(1);
-  let [morning, setmorning] = useState(false);
+  let [isMorning, setMorning] = useState();
 
   return ( 
     
-    <div className={`box ${morning ? 'dayLight' : ''}`}>
-      <h1>Day time = {morning ? 'morning' : 'night'}</h1>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Day time = {isMorning ? 'morning' : 'night'}</h1>
   <C counter={count} />
   <br/>
   <button onClick={() => setCount(count + 1)}>update counter</button>
-  
-  <button onClick={() => setmorning(!morning)}>update day</button>
+
+  <button onClick={() => setMorning(!morning)}>update day</button>
     </div>
   ); 
   }
