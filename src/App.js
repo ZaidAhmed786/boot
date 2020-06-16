dc512502f80e5a7045bbd32b3ffa8f1a0c55930a
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-16 15:35:10 +0500

    done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import C from './counter.js';
 import './App.css';
 function App() {
   let [count, setCount] = useState(1);
-  let [isMorning , setMorning] = useState();
+  let [isMorning , setMorning] = useState(true);
   return ( 
-    <div className={'box  ${isMorning ? 'dayLight' : ''}'}>
+    <div className={"box  ${isMorning ? 'day' : ''}"}>
       <h1>Day time = ${isMorning ? 'Morning' : 'Night'}</h1>
   <C counter={count} />
   <br/>
