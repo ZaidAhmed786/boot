e7062eb049ae158f7a4370eb2a6617b63d4559b1
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-16 15:02:40 +0500

    add new features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import C from './counter.js';
+import './App.css';
 function App() {
   let [count, setCount] = useState(1);
-
+  let [isMorning , setMorning] = useState(true);
   return ( 
-    <div>
+    <div className={'box ${isMorning ? "day" : ""}' }>
+      <h1>Day time = ${isMorning ? "Morning" : "Night"}</h1>
   <C counter={count} />
   <br/>
   <button onClick={() => setCount(count + 1)}>update counter</button>
-
+  <button onClick={() => setMorning(!isMorning)}>update counter</button>
     </div>
   );
 }
