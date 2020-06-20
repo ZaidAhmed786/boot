50d6ea144d8072fce28b0514888e88fc5aa331c7
ZaidAhmed786 <ZaidAhmed786@users.noreply.github.com>
2020-06-20 14:44:32 +0500

    wow

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Parent from './Shahid';
+import CounterContext from './Zaid';
 
 function App() {
     return(
+      <CounterContext.Provider value='zaid'>
       <div>
         <Parent name='zaid' />
       </div>
+      </CounterContext.Provider>
     )
       }
 export default App; 
--- src/Zaid.js
+++ src/Zaid.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CounterContext from './CounterContext';
+import counterContext from './CounterContext';
 
 
 const Child = (props) => {
-
+let counterValue = useContext(counterContext)
 
 return (
 
 <div>
 
     <h1>this is first child</h1>
-<h1>this is first child{props.name}</h1>
+<h1>this is first child : {counterContext}</h1>
 </div>
 
 )
